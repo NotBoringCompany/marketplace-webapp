994629e147a7a927d6dd69d14f30b2f8ab034220
shonsirsha <seangeekpro@gmail.com>
2022-02-18 23:33:31 +0100

    added progress bar on route chg

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import Router from "next/router";
+
 import { MoralisProvider } from "react-moralis";
 import { AppProvider } from "../context/AppContext";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 // import { useContext } from "react";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +11,16 @@ import "../styles/root.scss";
 import "../styles/styles.scss";
 
 function Main({ Component, pageProps }) {
+	NProgress.configure({
+		minimum: 0.3,
+		easing: "ease",
+		speed: 350,
+
+		showSpinner: false,
+	});
+	Router.events.on("routeChangeStart", () => NProgress.start());
+	Router.events.on("routeChangeComplete", () => NProgress.done());
+	Router.events.on("routeChangeError", () => NProgress.done());
 	return (
 		<MoralisProvider
 			appId={process.env.NEXT_PUBLIC_APP_ID}
