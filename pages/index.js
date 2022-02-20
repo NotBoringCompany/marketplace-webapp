6c419a5fec4339ce1064a467800f2936cb499b03
shonsirsha <seangeekpro@gmail.com>
2022-02-20 12:17:41 +0100

    able to get appropriate email and address of current user

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 
 import Container from "react-bootstrap/Container";
@@ -12,7 +13,11 @@ const StyledContainer = styled(Container)`
 `;
 
 export default function Home() {
-	const { isAuthenticated } = useMoralis();
+	const { isAuthenticated, account, user } = useMoralis();
+
+	useEffect(() => {
+		user && console.log(user.attributes);
+	}, [user]);
 
 	return (
 		<Layout>
@@ -20,6 +25,16 @@ export default function Home() {
 				<h2 className="text-white">
 					Home.. you are {isAuthenticated ? `signed in` : `not signed in`}
 				</h2>
+
+				{isAuthenticated && (
+					<p className="text-white">
+						Wallet Address: {user.attributes.ethAddress.toUpperCase()}
+						<br />
+						<hr />
+						Linked email:{" "}
+						{user.attributes.email ? user.attributes.email : "None."}
+					</p>
+				)}
 			</StyledContainer>
 		</Layout>
 	);
