276c5588133e9a090b50b6426f03e4591469cdc5
shonsirsha <seangeekpro@gmail.com>
2022-03-08 20:19:50 +0100

    rm unused

--- components/Filters/FertilityFilter.js
+++ components/Filters/FertilityFilter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import CollapseFilter from "components/CollapseFilter";
 import CustomSlider from "./CustomSlider";
 import RangeInput from "./RangeInput";
